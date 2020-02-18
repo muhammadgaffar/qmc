fae109240b2d76a5c1abde4c5c88cd27261c011c
mgaffar <gafar98@gmail.com>
2020-02-18 23:45:19 +0700

    remove unnecessary feature
    remove gf name, remove gf beta, remove precision, simplify syntax

--- gf.jl
+++ gf.jl
@@ -24,121 +24,121 @@ const iomega_n = iOmega_n()
 const iwn      = iOmega_n()
 Base.show(io::IO,D::iOmega_n) = print(io,"iωₙ")
 
-mutable struct GfrealFreq{T} <: GreenFunction
-    name::String
-    mesh::Vector{T}
+
+# a note, why the gf object is restricted to float64?
+# I know for it's good if we have flexibel precision like i was intended to be
+# but I began realized that this was a bad practice for general green function process
+# causing too much degree of freedom is not good
+# if we do really need to kills precision for better performance, use it in process that-
+# -takes much cpu time, usually involve high number of iteration such as monte carlo
+
+mutable struct GfrealFreq{N} <: GreenFunction
     orbs::Tuple
-    data::Array{Complex{T},3}
+    mesh::Vector{Float64}
+    data::Array{Complex{Float64},3}
 end
 
-mutable struct GfimFreq{T} <: GreenFunction
-    name::String
-    mesh::Vector{T}
+mutable struct GfimFreq{N} <: GreenFunction
     orbs::Tuple
-    data::Array{Complex{T},3}
-    beta::T
+    mesh::Vector{Float64}
+    data::Array{Complex{Float64},3}
 end
 
-mutable struct GfimTime{T} <: GreenFunction
-    name::String
-    mesh::Vector{T}
+mutable struct GfimTime{N} <: GreenFunction
     orbs::Tuple
-    data::Array{Complex{T},3}
-    beta::T
+    mesh::Vector{Float64}
+    data::Array{Complex{Float64},3}
 end
 
 ## for Gw
-function setGw(orbs::T1, nw::Int,wrange::T2;
-            name::String,precision=Float64) where {T1<:Tuple,T2<:Tuple}
+function setGw(orbs::T1, nw::Int,wrange::T2) where {T1<:Tuple,T2<:Tuple}
     w = LinRange(wrange...,nw)
-    initzeros = zeros(length(orbs),length(orbs), nw)
-    return GfrealFreq{precision}(name,w,orbs,initzeros)
+    norb = length(orbs)
+    initzeros = zeros(norb,norb, nw)
+    return GfrealFreq{norb}(orbs,w,initzeros)
 end
-setGw(;orbs,nw,wrange,name,precision=Float64) = setGw(orbs,nw,wrange,name=name,precision=precision)
+setGw(;orbs,nw,wrange) = setGw(orbs,nw,wrange)
 
-Base.show(io::IO,gf::GfrealFreq{T}) where T = print(io,
-"GfRealFreq{$T} : $(gf.name)
-    orbs($(length(gf.orbs))) : $(gf.orbs)
+Base.show(io::IO,gf::GfrealFreq{N}) where N = print(io,
+"Green Function{Real Frequency} :  Matrix$((length(gf.orbs),length(gf.orbs)))
+    orbs    : $(gf.orbs)
     mesh    : $(length(gf.mesh))
     range   : ($(gf.mesh[1]),$(gf.mesh[end]))")
 
 ## for Giwn
-function setGiwn(orbs::T,n::Int,beta::N;
-                name::String,precision=Float64) where {N<:Number,T<:Tuple}
+function setGiwn(orbs::T,n::Int,beta::R) where {T<:Tuple,R<:Real}
     wn = (2*collect(-n:n) .+ 1) * π / beta
+    norb = length(orbs)
     initzeros = zeros(length(orbs),length(orbs), 2n+1)
-    return GfimFreq{precision}(name,wn,orbs,initzeros,beta)
+    return GfimFreq{norb}(orbs,wn,initzeros)
 end
-setGiwn(;orbs,n,beta,name,precision=Float64) = setGiwn(orbs,n,beta,name=name,precision=precision)
+setGiwn(;orbs,n,beta) = setGiwn(orbs,n,beta)
 
-Base.show(io::IO,gf::GfimFreq{T}) where T = print(io,
-"GfimFreq{$T} : $(gf.name)
-    orbs($(length(gf.orbs))) : $(gf.orbs)
-    nwn     : $(Int(0.5*(length(gf.mesh)-1)))
-    beta    : $(gf.beta)")
+function Base.show(io::IO,gf::GfimFreq{N}) where N
+    nwn = Int(0.5*(length(gf.mesh)-1))
+    beta = π / gf.mesh[nwn+1]
+    print(io,
+"Green Function{Imaginary Frequency} : Matrix$((length(gf.orbs),length(gf.orbs)))
+    orbs    : $(gf.orbs)
+    Nwn     : $nwn
+    beta    : $beta")
+end
 
 ## for Gtau
-function setGtau(orbs::T,nslices::Int,beta::N;
-                name::String,precision=Float64) where {T<:Tuple,N<:Number}
+function setGtau(orbs::T,nslices::Int,beta::R) where {T<:Tuple,R<:Real}
     tau = LinRange(0,beta,nslices)
+    norb = length(orbs)
     initzeros = zeros(length(orbs),length(orbs), nslices)
-    return GfimTime{precision}(name,tau,orbs,initzeros,beta)
+    return GfimTime{norb}(orbs,tau,initzeros)
 end
-setGtau(;orbs,nslices,beta,name,precision=Float64) = setGtau(orbs,nslices,beta,name=name,precision=precision)
+setGtau(;orbs,nslices,beta) = setGtau(orbs,nslices,beta)
 
-Base.show(io::IO,gf::GfimTime{T}) where T = print(io,
-"GfimTime{$T} : $(gf.name)
-    orbs($(length(gf.orbs))) : $(gf.orbs)
-    L slices: $(length(gf.mesh))
-    beta    : $(gf.beta)")
+function Base.show(io::IO,gf::GfimTime{N}) where N
+    ntau = length(gf.mesh)
+     print(io,
+"GfimTime{Imaginary Time} : Matrix$((length(gf.orbs),length(gf.orbs)))
+    orbs    : $(gf.orbs)
+    Ntau    : $(length(gf.mesh))
+    beta    : $(gf.mesh[end])")
+end
 
 ## OPERATOR IN GREEN FUNCTION ===============================||
 
 # copy is one of the most fundamental operator
-function copy(G::GreenFunction)
-    g = (typeof(G) <: GfrealFreq ? typeof(G)(G.name,G.mesh,G.orbs,G.data) :
-        typeof(G)(G.name,G.mesh,G.orbs,G.data,G.beta))
-    return g
-end
+copy(G::GreenFunction) = typeof(G)(G.orbs,G.mesh,G.data)
 
 # addition operator is simply element-wise operation
 function +(G1::GreenFunction,G2::GreenFunction)
-    if typeof(G1) != typeof(G2) throw("you cannot add different type green function") end
-    g = copy(G1)
-    g.data = G1.data .+ G2.data; g.name = "GF"
+    if typeof(G1) != typeof(G2) throw("GF type/size MisMatch") end
+    g = copy(G1); g.data = G1.data .+ G2.data
     return g
 end
 
 # negative of operator is simply element-wise operation
 function -(G::GreenFunction)
-    g = copy(G)
-    g.data = -G.data
+    g = copy(G); g.data = -G.data
     return g
 end
 
 # substraction operator is simply element-wise operation
 # why not G1-G2 =  G1+(-G2), it takes more allocation because we use -G operator first.
 function -(G1::GreenFunction,G2::GreenFunction)
-    if typeof(G1) != typeof(G2) throw("you cannot add different type green function") end
-    g = copy(G1)
-    g.data = G1.data .- G2.data; g.name = "GF"
+    if typeof(G1) != typeof(G2) throw("GF type/size MisMatch") end
+    g = copy(G1); g.data = G1.data .- G2.data
     return g
 end
 
 # multiplication operator between GF is matrix multiplication
 function *(G1::GreenFunction,G2::GreenFunction)
-    if typeof(G1) != typeof(G2) throw("you cannot multiply different type green function") end
-    g = copy(G1)
-    g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
-    for i in 1:length(g.mesh) g.data[:,:,i] = G1.data[:,:,i] * G2.data[:,:,i] end
-    g.name = "GF"
+    if typeof(G1) != typeof(G2) throw("GF type/size MisMatch") end
+    g = copy(G1); g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
+    @inbounds for i in 1:length(g.mesh) g.data[:,:,i] = G1.data[:,:,i] * G2.data[:,:,i] end
     return g
 end
 
 # multiplication operator between GF and a number is element-wise operation
 function *(a::Number,G::GreenFunction)
-    g = copy(G)
-    g.data = a .* G.data; g.name = "GF"
+    g = copy(G); g.data = a .* G.data;
     return g
 end
 *(G::GreenFunction,a::Number) = *(a::Number,G::GreenFunction)
@@ -148,10 +148,8 @@ function *(v::AbstractVector,G::GreenFunction)
     if length(v) != length(G.mesh)
         throw("DimensionMisMatch: got a dimension with lengths $(length(v)) and $(length(G.mesh))")
     end
-    g = copy(G)
-    g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
-    for i in 1:length(g.mesh) g.data[:,:,i] = v[i] .* G.data[:,:,i] end
-    g.name = "GF"
+    g = copy(G); g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
+    @inbounds for i in 1:length(g.mesh) g.data[:,:,i] = v[i] .* G.data[:,:,i] end
     return g
 end
 *(G::GreenFunction,v::AbstractVector) = *(v::AbstractVector,G::GreenFunction)
@@ -159,27 +157,22 @@ end
 # divide operator between GF is matrix operation
 # G1 * inv(G2) more faster this way
 function /(G1::GreenFunction,G2::GreenFunction)
-    if typeof(G1) != typeof(G2) throw("you cannot divide different type green function") end
-    g = copy(G1)
-    g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
-    for i in 1:length(g.mesh) g.data[:,:,i] = G1.data[:,:,i] * inv(G2.data[:,:,i]) end
-    g.name = "GF"
+    if typeof(G1) != typeof(G2) throw("GF type/size MisMatch") end
+    g = copy(G1); g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
+    @inbounds for i in 1:length(g.mesh) g.data[:,:,i] = G1.data[:,:,i] * inv(G2.data[:,:,i]) end
     return g
 end
 
 # divide operator between GF and a number is a * inv(G)
 # this type operation is faster than [I]a / G
 function /(a::Number,G::GreenFunction)
-    g = copy(G)
-    g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
-    for i in 1:length(g.mesh) g.data[:,:,i] = a .* inv(G.data[:,:,i]) end
-    g.name = "GF"
+    g = copy(G); g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
+    @inbounds for i in 1:length(g.mesh) g.data[:,:,i] = a .* inv(G.data[:,:,i]) end
     return g
 end
 # this is simply element-wise operation
 function /(G::GreenFunction,a::Number)
-    g = copy(G)
-    g.data = G.data ./ a; g.name = "GF"
+    g = copy(G); g.data = G.data ./ a
     return g
 end
 
@@ -192,8 +185,7 @@ end
 function +(iwn::iOmega_n,G::GfimFreq)
     g = copy(G)
     ID = Matrix(I,size(G.data,1),size(G.data,2))
-    for i in 1:length(g.mesh) g.data[:,:,i] = 1im*G.mesh[i]*ID + G.data[:,:,i] end
-    g.name = "GF"
+    @inbounds for i in 1:length(g.mesh) g.data[:,:,i] = 1im*G.mesh[i]*ID + G.data[:,:,i] end
     return g
 end
 +(G::GfimFreq,iwn::iOmega_n) = +(iwn::iOmega_n,G::GfimFreq)
@@ -202,10 +194,8 @@ end
 
 # inverse of green function is matrix inversion
 function inv(G::GreenFunction)
-    g = copy(G)
-    g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
-    for i in 1:length(g.mesh) g.data[:,:,i] = inv(G.data[:,:,i]) end
-    g.name = "GF"
+    g = copy(G); g.data = zeros(size(g.data)...) # this is very weird, wth we need to init zeros first to make success.
+    @inbounds for i in 1:length(g.mesh) g.data[:,:,i] = inv(G.data[:,:,i]) end
     return g
 end
 
@@ -232,16 +222,17 @@ function setfromToy!(G0::GreenFunction,toy::Symbol)
             div = (A0w ./ (freq[iw] .- wmesh))
             G0.data[1,1,iw] = integrate1d(wmesh,div)
         end
-        for iorb in 2:length(G0.orbs)
+        @inbounds for iorb in 2:length(G0.orbs)
             G0.data[iorb,iorb,:] = G0.data[1,1,:]
         end
     elseif check <: GfimTime
+        beta = G0.mesh[end]
         for t in 1:length(G0.mesh)
-            fm = 1.0 ./ (exp.(G0.beta .* wmesh) .+ 1.0 )
+            fm = 1.0 ./ (exp.(beta .* wmesh) .+ 1.0 )
             intg = A0w .* (fm .- 1.0) .* exp.(-wmesh .* G0.mesh[t])
             G0.data[1,1,t] = integrate1d(wmesh,intg)
         end
-        for iorb in 2:length(G0.orbs)
+        @inbounds for iorb in 2:length(G0.orbs)
             G0.data[iorb,iorb,:] = G0.data[1,1,:]
         end
     end
@@ -258,7 +249,7 @@ A0bethe(w; t=0.5) = (abs(w) < 2t ? sqrt(4t^2 - w^2) / (2π*t^2) : 0)
 function invFourier(Giwn::GfimFreq)
     # data and parameter
     nwn  = Int(0.5*(length(Giwn.mesh)-1))
-    beta = Giwn.beta
+    beta = π / Giwn.mesh[nwn+1]
 
     tau = LinRange(0,beta,nwn)
     gtau = zeros(eltype(Giwn.data), length(Giwn.orbs),length(Giwn.orbs), length(tau))
@@ -275,24 +266,25 @@ function invFourier(Giwn::GfimFreq)
         gtau[iorb,jorb,:] = spl.(tau)
     end
 
-    return GfimTime{eltype(Giwn.mesh)}(Giwn.name,tau,Giwn.orbs,gtau,beta)
+    return GfimTime{length(Giwn.orbs)}(Giwn.orbs,tau,gtau)
 end
 
 ### G(iωₙ) = Fourier(G(τ))
 function Fourier(Gtau::GfimTime)
     # parameter
     n = length(Gtau.mesh)
+    beta = Gtau.mesh[end]
 
     # allocate giwn
     giwn = zeros(eltype(Gtau.data), length(Gtau.orbs), length(Gtau.orbs), 2n+1)
-    wn = (2*collect(-n:n) .+ 1) * π / Gtau.beta
+    wn = (2*collect(-n:n) .+ 1) * π / beta
 
     # get G(iωₙ), not using fft.
     for iorb in 1:length(Giwn.orbs), jorb in 1:length(Giwn.orbs)
-        _,giwn[iorb,jorb,:] = ftau_to_fwn(Gtau.data[iorb,jorb,:],Gtau.mesh,Gtau.beta)
+        _,giwn[iorb,jorb,:] = ftau_to_fwn(Gtau.data[iorb,jorb,:],Gtau.mesh, beta)
     end
 
-    return GfimFreq{eltype(Gtau.mesh)}(Gtau.name,wn,Gtau.orbs,giwn,Gtau.beta)
+    return GfimFreq{eltype(Gtau.mesh)}(Gtau.orbs,wn,giwn)
 end
 
 ## G(ω) = Analytical Continuation G(iωₙ), using Pade.
@@ -307,11 +299,6 @@ function setfromPade(Giwn::GfimFreq; nw::Int, wrange::T,
     w = eltype(wn).(w)
 
     # matsubara points to samplings pade recursion,
-    # if too large, or precision too low there are numerical error, NAN.
-    # warn my precision
-    if eltype(Giwn.mesh) == Float32
-        @warn "Your precision is Float32, too low. use Float64, for more accurate result."
-    end
     npoints = (npoints == nothing ? length(wn) : npoints)
 
     gw = zeros(eltype(Giwn.data), length(Giwn.orbs),length(Giwn.orbs), length(w))
@@ -325,5 +312,5 @@ function setfromPade(Giwn::GfimFreq; nw::Int, wrange::T,
         gw[iorb,jorb,:] = pade_recursion(w, wn,gwn,npoints,coeff)
     end
 
-    return GfrealFreq{eltype(Giwn.mesh)}(Giwn.name,real(w),Giwn.orbs,gw)
+    return GfrealFreq{eltype(Giwn.mesh)}(Giwn.orbs,real(w),gw)
 end
--- test.jl
+++ test.jl
@@ -7,139 +7,85 @@ include("transform.jl")
 include("util.jl")
 
 # TEST GREEN FUNCTION ALLOCATION
-@testset "GfreFreq Allocation" begin
-    gw = setGw((1,2,3),501,(-2,2),name="nonInteracting")
-    @test eltype(gw.mesh) == Float64
+@testset "GF Allocation" begin
+    gw = setGw((1,2,3), 501,(-2,2))
     @test size(gw.data) == (3,3,501)
-    gw2 = setGw(orbs=(1,2,3),nw=501,wrange=(-2,2),name="nonInteracting")
-    @test gw2.mesh == gw.mesh
-    @test gw2.data == gw.data
-    println("Checking memory and time allocation :")
-    @time gw = setGw((1,2,3),501,(-2,2),name="nonInteracting")
-    println("Checking show output : ")
-    @show gw
-end;
-
-@testset "GfimFreq Allocation" begin
-    gf = setGiwn(("e2g","t2g"),2^8,50,name="nonInteracting")
-    @test eltype(gf.mesh) == Float64
-    @test size(gf.data) == (2,2,2*2^8+1)
-    gf2 = setGiwn(orbs=("e2g","t2g"),n=2^8,beta=50,name="nonInteracting")
-    @test gf2.mesh == gf.mesh
-    @test gf2.data == gf.data
-    println("Checking memory and time allocation :")
-    @time gf = setGiwn(("e2g","t2g"),2^8,50,name="nonInteracting")
-    println("Checking show output : ")
-    @show gf
-end;
-
-@testset "GfimTime Allocation" begin
-    gf = setGtau(("e2g","t2g"),2^8,50,name="nonInteracting")
-    @test eltype(gf.mesh) == Float64
-    @test size(gf.data) == (2,2,2^8)
-    gf2 = setGtau(orbs=("e2g","t2g"),nslices=2^8,beta=50,name="nonInteracting")
-    @test gf2.mesh == gf.mesh
-    @test gf2.data == gf.data
-    println("Checking memory and time allocation :")
-    @time gf = setGtau(("e2g","t2g"),2^8,50,name="nonInteracting")
-    println("Checking show output : ")
-    @show gf
+    giw = setGiwn(("e2g","t2g"), 2^8,50)
+    @test size(giw.data) == (2,2,2*2^8+1)
+    gt = setGtau(("s", 2),2487,50)
+    @test size(gt.data) == (2,2,2487)
 end;
 
 # TEST GREEN FUNCTION OPERATOR
 @testset "Operator" begin
-    gf = setGtau(("e2g","t2g"),2^8,50,name="gtest1")
+    gf = setGtau(("e2g","t2g"),2^8,50)
     gf.data = rand(ComplexF64,size(gf.data)...)
-    gf2 = setGtau(("e2g","t2g"),2^8,50,name="gtest2")
+    gf2 = setGtau(("e2g","t2g"),2^8,50)
     gf2.data = rand(ComplexF64,size(gf.data)...)
 
     gf2 = copy(gf)
     @test size(gf2.data) == size(gf.data)
     @test size(gf2.mesh) == size(gf.mesh)
-    println("Checking memory and time allocation : copy")
-    @time gf2 = copy(gf)
 
     g = gf + gf2
     @test g.data == gf.data .+ gf2.data
-    println("Checking memory and time allocation : G1+G2")
-    @time gf + gf2
 
     g = -gf2
     @test g.data == -gf2.data
-    println("Checking memory and time allocation : -G1")
-    @time -gf2
 
     g = gf - gf2
     @test g.data == gf.data .- gf2.data
-    println("Checking memory and time allocation : G1-G2")
-    @time gf - gf2
 
     g = gf * gf2
     @test g.data[:,:,174] == gf.data[:,:,174] * gf2.data[:,:,174]
-    println("Checking memory and time allocation : G1*G2")
-    @time gf * gf2
 
     g = (2.0 + 3im) * gf
     g2 = gf * (2.0 + 3im)
     @test g.data == (2.0 + 3im) .* gf.data
     @test g.data == g2.data
-    println("Checking memory and time allocation : aG")
-    @time (2.0 + 3im) * gf
 
     v = rand(length(gf.mesh))
     g = v * gf
     g2 = gf * v
     @test g.data[:,:,54] == v[54] .* gf.data[:,:,54]
     @test g.data == g2.data
-    println("Checking memory and time allocation : vG")
-    @time v * gf
 
     g = gf / gf2
     @test g.data[:,:,174] ≈ gf.data[:,:,174] / gf2.data[:,:,174]
-    println("Checking memory and time allocation : G1/G2")
-    @time gf / gf2
 
     g = 89/gf
     @test g.data[:,:,98] == 89 .* inv( gf.data[:,:,98] )
-    println("Checking memory and time allocation : a/G")
-    @time 89 / gf
 
     g = gf2 / 24
     @test g.data[:,:,148] == gf.data[:,:,148] ./ 24
-    println("Checking memory and time allocation : G1/a")
-    @time gf2 / 24
 
-    gf = setGiwn(("s","p"), 128, 40, name="GF")
+    gf = setGiwn(("s","p"), 128, 40)
     gf.data = rand(ComplexF64,size(gf.data)...)
     g = iwn + 5gf
     @test g.data[:,:,2] == 1im*gf.mesh[2]*Matrix(I,2,2) + 5 * gf.data[:,:,2]
     g =  8 / gf - iwn
     @test g.data[:,:,87] == 8 .* inv(gf.data[:,:,87]) - 1im*gf.mesh[87]*Matrix(I,2,2)
-    println("Checking memory and time allocation : iwn +- G")
-    @time iwn + gf
 
     g = inv(gf)
     @test g.data[:,:,111] == inv(gf.data[:,:,111])
-    println("Checking memory and time allocation : G^{-1}")
-    @time g = inv(gf)
 end;
 
 
 # TEST GREEN FUNCTION TRANSFORMATION
 # CONFIRMATION WITH PLOT
-G0w = setGw(("up","dw"),501,(-2,2),name="nonInteracting")
+G0w = setGw(("up","dw"),501,(-2,2))
 setfromToy!(G0w,:bethe)
 re_pgw = plot(G0w.mesh, real(G0w.data[1,1,:]),label="real_up")
 plot!(re_pgw,G0w.mesh, real(G0w.data[2,2,:]),label="real_dw")
 im_pgw = plot(G0w.mesh, imag(G0w.data[1,1,:]),label="imag_up")
 plot!(im_pgw,G0w.mesh, imag(G0w.data[2,2,:]), label="imag_dw")
 
-Giwn = setGiwn(("up","dw"),256,20.0,name="nonInteracting")
+Giwn = setGiwn(("up","dw"),256,20.0)
 setfromToy!(Giwn,:bethe)
 pgiw = plot(Giwn.mesh,imag(Giwn.data[1,1,:]),label="imag_up", xlims=(-10,10))
 plot!(pgiw,Giwn.mesh,imag(Giwn.data[2,2,:]),label="imag_dw", xlims=(-10,10))
 
-Gtau = setGtau(("s","p"),256,20,name="nonInteracting")
+Gtau = setGtau(("s","p"),256,20)
 setfromToy!(Gtau,:bethe)
 pgtau = plot(Gtau.mesh,real(Gtau.data[1,1,:]),label="up")
 plot!(pgtau,Gtau.mesh,real(Gtau.data[2,2,:]),label="dw")
